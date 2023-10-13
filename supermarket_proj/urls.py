4682351a956e90f08adcae5295716010b3abfae5
eladh23 <eladelad121212@gmail.com>
2023-10-14 00:55:56 +0300

    some update

--- supermarket_proj/urls.py
+++ supermarket_proj/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('product', views.products, name="products"), 
     path('product/<id>', views.product_detail, name="product_detail"),  
-    
+    path('carts', views.carts, name="carts"),
+    path('cart/<int:cart_id>/items', views.cart_items, name="cart_items"),
 ]
--- supermarket_proj/views.py
+++ supermarket_proj/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ProductSerializer
-from .models import Product
+from .serializers import CartItemSerializer, CartSerializer, ProductSerializer
+from .models import Cart, CartItem, Product
 
 @api_view(['GET', 'POST'])
 def products(request):
@@ -36,4 +36,30 @@ def product_detail(request, id):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            
+@api_view(['GET', 'POST'])
+def carts(request):
+    if request.method == 'GET':
+        # Retrieve the user's cart if it exists
+        user = request.user  # Assuming you are using authentication
+        try:
+            cart = Cart.objects.get(user=user)
+            serializer = CartSerializer(cart)
+            return Response(serializer.data)
+        except Cart.DoesNotExist:
+            # If the user doesn't have a cart, you can return an empty cart or an appropriate response
+            return Response({"message": "Cart not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    elif request.method == 'POST':
+        # Create a new cart for the user
+        user = request.user  # Assuming you are using authentication
+        cart = Cart(user=user)
+        cart.save()
+        serializer = CartSerializer(cart)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)   
+
+@api_view(['GET'])
+def cart_items(request, cart_id):
+    # Retrieve all cart items for a specific cart
+    cart_items = CartItem.objects.filter(cart=cart_id)
+    cart_items_serialized = CartItemSerializer(cart_items, many=True).data
+    return Response(cart_items_serialized)        
