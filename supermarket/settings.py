561d7192fa919ddae6078d0639744c4777baa3d1
eladh23 <eladelad121212@gmail.com>
2023-11-24 11:16:59 +0200

    new db

--- supermarket/settings.py
+++ supermarket/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'supermarket.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+        
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
